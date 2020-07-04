d68aec99ca322a94381144c0ac30595d74e72225
zhangzhenyan <120157229@qq.com>
2020-07-04 22:25:22 +0800

    commit push

---
+++ cat/src/main/java/com/rain/controller/KafkaController.java
@@ -0,0 +1,54 @@
+package com.rain.controller;
+
+import com.rain.annotation.aopLog.Loggable;
+import com.rain.responseVo.ResultVo;
+import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * kafka controller
+ *
+ * @author lazy cat
+ * @since 2019-04-11
+ **/
+@Api(description = "kafka controller")
+@RestController
+@RequestMapping(value = "/kafka")
+@Loggable(loggable = true)
+public class KafkaController extends BaseController {
+
+    @Resource
+    private KafkaTemplate<Integer, String> kafkaTemplate;
+
+    private Logger log = LoggerFactory.getLogger(KafkaController.class);
+
+    /**
+     * 测试前需要开启zookeeper服务和kafka服务以及kafkaListener
+     *
+     * @see com.rain.config.kafkaListener
+     **/
+    @PostMapping("/testKafka")
+    public ResultVo testKafka(String data) {
+        ListenableFuture<SendResult<Integer, String>> send = kafkaTemplate.send("hello", data);
+        send.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+            public void onFailure(Throwable throwable) {
+                log.info("send fail!");
+            }
+
+            public void onSuccess(SendResult<Integer, String> integerStringSendResult) {
+                log.info("send success!");
+            }
+        });
+        return ResultVo.success(data, "send success!");
+    }
+}
---
+++ common/src/main/java/com/rain/config/KafkaConfig.java
@@ -0,0 +1,77 @@
+package com.rain.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * KafkaConfig
+ *
+ * @author lazy cat
+ * @since 2019-07-4
+ **/
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    @Value("${kafka.ip}")
+    private String kafkaIp;
+
+    //kafkaTemplate实现了Kafka发送接收等功能
+    @Bean
+    public KafkaTemplate<Integer, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    //ConcurrentKafkaListenerContainerFactory为创建Kafka监听器的工程类，这里只配置了消费者
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<Integer, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    //生产者工厂
+    private ProducerFactory<Integer, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    //消费者工厂
+    @Bean
+    public ConsumerFactory<Integer, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    //生产者配置
+    private Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put("bootstrap.servers", kafkaIp);
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return props;
+    }
+
+    //消费者配置
+    private Map<String, Object> consumerConfigs() {
+        HashMap<String, Object> props = new HashMap<>();
+        props.put("bootstrap.servers", kafkaIp);
+        props.put("group.id", "hello");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        return props;
+    }
+
+}
