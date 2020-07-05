85a3df72095c726e3874deb84325270a7f18c42d
zhangzhenyan <120157229@qq.com>
2020-07-05 18:21:07 +0800

    commit push

---
+++ cat/src/main/java/com/rain/study/net/bio/BioClients.java
@@ -0,0 +1,25 @@
+package com.rain.study.net.bio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * bio client
+ *
+ * @author lazy cat
+ * 2020-07-05
+ **/
+public class BioClients {
+
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket("127.0.0.1", 9999);
+        OutputStream os = socket.getOutputStream();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入您要发送的内容ya:");
+        os.write(scanner.next().getBytes());
+        socket.close();
+    }
+}
---
+++ cat/src/main/java/com/rain/study/net/bio/BioServer.java
@@ -0,0 +1,41 @@
+package com.rain.study.net.bio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * bio server
+ *
+ * @author lazy cat
+ * 2020-07-05
+ **/
+public class BioServer {
+
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = new byte[1024];
+
+        ServerSocket serverSocket = new ServerSocket();
+        serverSocket.bind(new InetSocketAddress("127.0.0.1", 9999));
+        while (true) {
+            System.out.println("wait connect");
+            Socket socket = serverSocket.accept();
+            System.out.println("connect success");
+            new Thread(() -> {
+                int read = 0;
+                System.out.println("wait client write");
+                try {
+                    read = socket.getInputStream().read(bytes);
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+                System.out.println("server read success");
+                if (read > 0) {
+                    System.out.println(new String(bytes, 0, read, Charset.forName("utf-8")));
+                }
+            }).start();
+        }
+    }
+}
