fda494e027cd3f43df466fbe5f9f48db58eb5484
zhangzhenyan <120157229@qq.com>
2020-07-06 23:56:00 +0800

    commit push

--- apple/src/main/java/com/rain/controller/RemoteController.java
+++ apple/src/main/java/com/rain/controller/RemoteController.java
@@ -1,10 +1,15 @@
 package com.rain.controller;
 
+import com.rain.api.apple.model.User;
+import com.rain.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * remote controller
@@ -19,10 +24,27 @@ public class RemoteController {
     @Value("${server.port}")
     String port;
 
+    @Resource
+    private UserService userService;
+
     //http://localhost:8762/apple/fruit/hi?name=zzy
     @GetMapping("/hi")
     public String home(@RequestParam String name) {
         System.out.println(name + " come to client");
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    //http://localhost:8762/apple/fruit/select
+    @GetMapping("/select")
+    public User select() {
+        User user = new User();
+        user.setId("i am id");
+        user.setName("zzy");
+        return user;
+    }
+
+    @GetMapping(value = "/list")
+    public List<User> list(User user) {
+        return userService.select(user);
     }
 }
--- banana/src/main/java/com/rain/controller/RestTemplateController.java
+++ banana/src/main/java/com/rain/controller/RestTemplateController.java
@@ -1,5 +1,6 @@
 package com.rain.controller;
 
+import com.rain.api.apple.model.User;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * restTemplate controller
@@ -26,8 +29,32 @@ public class RestTemplateController {
     @Value("${banana.appleHi}")
     private String url;
 
-    @GetMapping(value = "/list")
-    public String list(String name) {
+    @GetMapping(value = "/getString")
+    public String getString(String name) {
         return restTemplate.getForObject(url + name, String.class);
+    }
+
+    @GetMapping(value = "/getObject")
+    public User getObject() {
+        final String GET_OBJECT = "http://127.0.0.1:8762/apple/fruit/select";
+        return restTemplate.getForObject(GET_OBJECT, User.class);
+    }
+
+    @GetMapping(value = "/getList")
+    public List<User> getList() {
+        final String GET_LIST = "http://127.0.0.1:8762/apple/fruit/list";
+        User[] users = restTemplate.getForObject(GET_LIST, User[].class);
+        if (users != null)
+            return Arrays.asList(users);
+        return null;
+    }
+
+    @GetMapping(value = "/getAndHaveArgs")
+    public List<User> getAndHaveArgs() {
+        final String GET_AND_HAVE_ARGS = "http://127.0.0.1:8762/apple/fruit/list?name=zzy";
+        User[] users = restTemplate.getForObject(GET_AND_HAVE_ARGS, User[].class);
+        if (users != null)
+            return Arrays.asList(users);
+        return null;
     }
 }
