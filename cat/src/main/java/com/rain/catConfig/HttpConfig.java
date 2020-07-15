ed240e76519bd7a7d731357ab7b0651d52bb3f39
zhangzhenyan <120157229@qq.com>
2020-07-15 11:29:14 +0800

    commit push

---
+++ cat/src/main/java/com/rain/catConfig/HttpConfig.java
@@ -0,0 +1,34 @@
+package com.rain.catConfig;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SpringBoot同时支持https和http访问
+ *
+ * @author lazy cat
+ * 2020-07-15
+ **/
+@Configuration
+public class HttpConfig {
+
+    @Value("${http.port}")
+    private Integer port;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(port);
+        return connector;
+    }
+}
