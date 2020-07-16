3b295ab47df186723cc95b00b4a6354bd25932ae
zhangzhenyan <120157229@qq.com>
2020-07-16 15:36:27 +0800

    commit push

--- apple/src/main/java/com/rain/controller/RedisController.java
+++ apple/src/main/java/com/rain/controller/RedisController.java
@@ -6,7 +6,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,29 +40,32 @@ public class RedisController {
     @Resource
     private RedisTemplate<String, String> redisTemplate;
 
-    /**
-     * 演示redis的使用
-     **/
-    @PostMapping("/testRedis")
-    public void testRedis() {
-        //测试存储string
+    @ApiOperation("测试存储string")
+    @PostMapping(value = "saveString")
+    public void saveString() {
         String key = StringUtils.join(new String[]{"user", "token"}, ":");
         String value = "0171826834da554b43d2c72bb1767c7898f27bf91775463e2b3b4e0f3806e0255d6e52ca286b";
         stringRedisTemplate.opsForValue().set(key, value);
         System.out.println(stringRedisTemplate.opsForValue().get(key));
         stringRedisTemplate.expire(key, 60, TimeUnit.SECONDS);
+    }
 
-        //测试存储Object
+    @ApiOperation("测试存储Object")
+    @PostMapping(value = "saveObject")
+    public void saveObject() {
+        String value = "0171826834da554b43d2c72bb1767c7898f27bf91775463e2b3b4e0f3806e0255d6e52ca286b";
         User user = new User();
         user.setName("Tom");
         user.setPassword(value);
         String userKey = StringUtils.join(new String[]{"user", "model"}, ":");
-        redisCacheTemplate.opsForValue().set(userKey, user);
+        redisCacheTemplate.opsForValue().set(userKey, user, 60, TimeUnit.SECONDS);
         User demo = (User) redisCacheTemplate.opsForValue().get(userKey);
         System.out.println(demo);
-        stringRedisTemplate.expire(userKey, 60, TimeUnit.SECONDS);
+    }
 
-        //测试存储list
+    @ApiOperation("测试存储list")
+    @PostMapping("/saveList")
+    public void saveList() {
         String listKey = StringUtils.join(new String[]{"user", "list"}, ":");
         List<User> list = new ArrayList<>();
         User user1 = new User();
@@ -75,41 +77,18 @@ public class RedisController {
         list.add(user1);
         list.add(user2);
 
-        redisCacheTemplate.boundValueOps(listKey).set(list);
+        redisCacheTemplate.boundValueOps(listKey).set(list, 60, TimeUnit.SECONDS);
         List<User> userList = (List<User>) redisCacheTemplate.boundValueOps(listKey).get();
         if (null != userList && userList.size() > 0) {
             for (User n : userList) {
                 System.out.println(n);
             }
         }
-        stringRedisTemplate.expire(listKey, 60, TimeUnit.SECONDS);
     }
 
     @ApiOperation("测试redis的setNx命令")
     @GetMapping(value = "setNx")
     public Boolean setNx() {
         return stringRedisTemplate.opsForValue().setIfAbsent("lock", "1", 20, TimeUnit.SECONDS);
-    }
-
-    /**
-     * 演示redis的使用
-     **/
-    @PostMapping("/testRedisTwo")
-    public void testRedisTwo() {
-        ListOperations<String, String> listOperations = redisTemplate.opsForList();
-        List<String> strings = new ArrayList<>();
-        strings.add("鬼泣5");
-        strings.add("荒野大镖客2");
-        strings.add("仙剑奇侠传7");
-        listOperations.leftPushAll("list", strings);
-        listOperations.leftPush("list", "hello");
-        listOperations.rightPush("list", "redis");
-        Long size = redisTemplate.opsForList().size("list");
-        // 从list取数据，list 是 key， 第二个参数是开始取的下标，从0开始，第三个参数是结束的下标
-        List<String> list = null;
-        if (size != null)
-            list = listOperations.range("list", 0, size);
-        if (list != null)
-            System.out.println(list.toString());
     }
 }
