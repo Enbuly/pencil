802a3cf2eec7d8c77fe44edfbdd6ecd826d1fede
zhangzhenyan <120157229@qq.com>
2020-07-16 15:45:12 +0800

    commit push

--- apple/src/main/java/com/rain/controller/AsyncController.java
+++ apple/src/main/java/com/rain/controller/AsyncController.java
@@ -1,16 +1,9 @@
 package com.rain.controller;
 
 import com.rain.annotation.aopLog.Loggable;
-import com.rain.annotation.limit.RateLimit;
 import com.rain.responseVo.ResultVo;
 import com.rain.service.ThreadServer;
 import io.swagger.annotations.Api;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
