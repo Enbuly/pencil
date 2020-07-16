08dac674569733c26e74b6c78ae0fa53f3819e49
zhangzhenyan <120157229@qq.com>
2020-07-16 18:51:58 +0800

    commit push

--- dog/src/main/java/com/rain/controller/RedissonController.java
+++ dog/src/main/java/com/rain/controller/RedissonController.java
@@ -1,12 +1,19 @@
 package com.rain.controller;
 
+import com.rain.api.apple.model.User;
 import com.rain.responseVo.ResultVo;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +27,13 @@ public class RedissonController extends BaseController {
 
     @Resource
     private RedissonClient redissonClient;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    @Qualifier("redisCacheTemplate")
+    private RedisTemplate<String, Object> redisCacheTemplate;
 
     private Logger log = LoggerFactory.getLogger(RedissonController.class);
 
@@ -45,5 +59,27 @@ public class RedissonController extends BaseController {
         lock.lock(20, TimeUnit.SECONDS);
         log.info("lock it success!");
         lock.unlock();
+    }
+
+    @ApiOperation("测试存储string")
+    @PostMapping(value = "saveString")
+    public String saveString() {
+        String key = StringUtils.join(new String[]{"user", "token"}, ":");
+        String value = "123";
+        stringRedisTemplate.opsForValue().set(key, value);
+        stringRedisTemplate.expire(key, 60, TimeUnit.SECONDS);
+        return stringRedisTemplate.opsForValue().get(key);
+    }
+
+    @ApiOperation("测试存储Object")
+    @PostMapping(value = "saveObject")
+    public User saveObject() {
+        String value = "13828831312";
+        User user = new User();
+        user.setName("lazyCat");
+        user.setPhone(value);
+        String userKey = StringUtils.join(new String[]{"user", "model"}, ":");
+        redisCacheTemplate.opsForValue().set(userKey, user, 30, TimeUnit.SECONDS);
+        return (User) redisCacheTemplate.opsForValue().get(userKey);
     }
 }
