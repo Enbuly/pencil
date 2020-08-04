b817e70ae49aac985f2537f28f827a36e6b8dedf
zhangzhenyan <120157229@qq.com>
2020-08-04 19:41:59 +0800

    commit update

--- common/src/main/java/com/rain/config/DruidConfig.java
+++ common/src/main/java/com/rain/config/DruidConfig.java
@@ -8,9 +8,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
@@ -20,170 +21,22 @@ import org.springframework.transaction.support.TransactionTemplate;
  * @author lazy cat
  * @since  2019-04-11
  **/
-@Component
-@ConfigurationProperties(prefix = "spring.datasource")
+@Configuration
 @MapperScan(value = {"com.rain.dao"}, sqlSessionFactoryRef = "dbOneSqlSessionFactory")
 public class DruidConfig {
 
     private static final String MAPPER_LOCATION = "classpath:mapper/*.xml";
 
-    private String driverClassName;
-    private String url;
-    private String username;
-    private String password;
-    private int initialSize;
-    private int minIdle;
-    private int maxActive;
-    private long maxWait;
-    private long timeBetweenEvictionRunsMillis;
-    private long minEvictableIdleTimeMillis;
-    private String validationQuery;
-    private boolean testWhileIdle;
-    private boolean testOnBorrow;
-    private boolean testOnReturn;
-    private boolean poolPreparedStatements;
-    private int maxPoolPreparedStatementPerConnectionSize;
-
-    public String getDriverClassName() {
-        return driverClassName;
-    }
-
-    public void setDriverClassName(String driverClassName) {
-        this.driverClassName = driverClassName;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getInitialSize() {
-        return initialSize;
-    }
-
-    public void setInitialSize(int initialSize) {
-        this.initialSize = initialSize;
-    }
-
-    public int getMinIdle() {
-        return minIdle;
-    }
-
-    public void setMinIdle(int minIdle) {
-        this.minIdle = minIdle;
-    }
-
-    public int getMaxActive() {
-        return maxActive;
-    }
-
-    public void setMaxActive(int maxActive) {
-        this.maxActive = maxActive;
-    }
-
-    public long getMaxWait() {
-        return maxWait;
-    }
-
-    public void setMaxWait(long maxWait) {
-        this.maxWait = maxWait;
-    }
-
-    public long getTimeBetweenEvictionRunsMillis() {
-        return timeBetweenEvictionRunsMillis;
-    }
-
-    public void setTimeBetweenEvictionRunsMillis(long timeBetweenEvictionRunsMillis) {
-        this.timeBetweenEvictionRunsMillis = timeBetweenEvictionRunsMillis;
-    }
-
-    public long getMinEvictableIdleTimeMillis() {
-        return minEvictableIdleTimeMillis;
-    }
-
-    public void setMinEvictableIdleTimeMillis(long minEvictableIdleTimeMillis) {
-        this.minEvictableIdleTimeMillis = minEvictableIdleTimeMillis;
-    }
-
-    public String getValidationQuery() {
-        return validationQuery;
-    }
-
-    public void setValidationQuery(String validationQuery) {
-        this.validationQuery = validationQuery;
-    }
-
-    public boolean isTestWhileIdle() {
-        return testWhileIdle;
-    }
-
-    public void setTestWhileIdle(boolean testWhileIdle) {
-        this.testWhileIdle = testWhileIdle;
-    }
-
-    public boolean isTestOnBorrow() {
-        return testOnBorrow;
-    }
-
-    public void setTestOnBorrow(boolean testOnBorrow) {
-        this.testOnBorrow = testOnBorrow;
-    }
-
-    public boolean isTestOnReturn() {
-        return testOnReturn;
-    }
-
-    public void setTestOnReturn(boolean testOnReturn) {
-        this.testOnReturn = testOnReturn;
-    }
-
-    public boolean isPoolPreparedStatements() {
-        return poolPreparedStatements;
-    }
-
-    public void setPoolPreparedStatements(boolean poolPreparedStatements) {
-        this.poolPreparedStatements = poolPreparedStatements;
-    }
-
-    public int getMaxPoolPreparedStatementPerConnectionSize() {
-        return maxPoolPreparedStatementPerConnectionSize;
-    }
-
-    public void setMaxPoolPreparedStatementPerConnectionSize(int maxPoolPreparedStatementPerConnectionSize) {
-        this.maxPoolPreparedStatementPerConnectionSize = maxPoolPreparedStatementPerConnectionSize;
-    }
-
-    @Bean(name = "dbOneDruidDataSource", initMethod = "init", destroyMethod = "close")
+    @Bean(name = "dbOneDruidDataSource")
     @ConditionalOnMissingBean(name = "dbOneDruidDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
     public DruidDataSource dataSource() {
-        return copy(driverClassName, url, username,
-                password, initialSize, minIdle, maxActive, maxWait,
-                timeBetweenEvictionRunsMillis, minEvictableIdleTimeMillis, validationQuery,
-                testWhileIdle, testOnBorrow, testOnReturn,
-                poolPreparedStatements, maxPoolPreparedStatementPerConnectionSize);
+        return new DruidDataSource();
     }
 
     @Bean(name = "dbOneTransactionManager")
     @ConditionalOnMissingBean(name = "dbOneTransactionManager")
+    @Primary
     public DataSourceTransactionManager dbOneTransactionManager(@Qualifier("dbOneDruidDataSource") DruidDataSource druidDataSource) {
         return new DataSourceTransactionManager(druidDataSource);
     }
@@ -203,32 +56,5 @@ public class DruidConfig {
         sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver()
                 .getResources(MAPPER_LOCATION));
         return sessionFactory.getObject();
-    }
-
-    private DruidDataSource copy(String driverClassName, String url, String username,
-                                 String password, int initialSize, int minIdle, int maxActive, long maxWait,
-                                 long timeBetweenEvictionRunsMillis, long minEvictableIdleTimeMillis, String validationQuery,
-                                 boolean testWhileIdle, boolean testOnBorrow, boolean testOnReturn,
-                                 boolean poolPreparedStatements, int maxPoolPreparedStatementPerConnectionSize) {
-        DruidDataSource dataSource = new DruidDataSource();
-
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        dataSource.setInitialSize(initialSize);
-        dataSource.setMinIdle(minIdle);
-        dataSource.setMaxActive(maxActive);
-        dataSource.setMaxWait(maxWait);
-        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
-        dataSource.setValidationQuery(validationQuery);
-        dataSource.setTestWhileIdle(testWhileIdle);
-        dataSource.setTestOnBorrow(testOnBorrow);
-        dataSource.setTestOnReturn(testOnReturn);
-        dataSource.setPoolPreparedStatements(poolPreparedStatements);
-        dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
-
-        return dataSource;
     }
 }
