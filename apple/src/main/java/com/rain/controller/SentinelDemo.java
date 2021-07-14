6afe9a591ec9c05844fbfbaab95ab486be76974e
zhangzhenyan <enbuly.zhang@goldentec.com>
2021-07-14 10:39:49 +0800

    commit

---
+++ apple/src/main/java/com/rain/controller/SentinelDemo.java
@@ -0,0 +1,45 @@
+package com.rain.controller;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * sentinel
+ *
+ * @author zhangzhenyan
+ * 2021-07-14
+ **/
+public class SentinelDemo {
+    public static void main(String[] args) {
+        // 配置规则.
+        initFlowRules();
+
+        while (true) {
+            // 1.5.0 版本开始可以直接利用 try-with-resources 特性，自动 exit entry
+            try (Entry entry = SphU.entry("HelloWorld")) {
+                // 被保护的逻辑
+                System.out.println("hello world");
+            } catch (Exception ex) {
+                // 处理被流控的逻辑
+                System.out.println("blocked!");
+            }
+        }
+    }
+
+    private static void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(20);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
